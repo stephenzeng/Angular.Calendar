37602bc193e54696ef259395225a06e850a6778a
zeng.stephen@gmail.com <zeng.stephen@gmail.com>
2014-10-16 00:00:24 +0800

    Adding datetime picker

--- js/datepickerApp.js
+++ js/datepickerApp.js
@@ -4,33 +4,50 @@
 'use strict'
 var datepickerApp = angular.module('datepickerApp', []);
 
-$('input.datepicker').datepicker({format: 'dd/m/yyyy'});
-$('input.timepicker').timepicker({scrollbar: true, timeFormat: 'HH:mm'});
-
 datepickerApp.controller('datepickerController', function ($scope, $log) {
-    $scope.selectedDatetime = moment();
-    $scope.selectedDate = $scope.selectedDatetime.format('DD/MM/YYYY')
-    $scope.selectedTime = $scope.selectedDatetime.format('HH:mm');
-
     $scope.reset = function () {
         $scope.selectedDatetime = moment();
+        $scope.selectedDate = $scope.selectedDatetime.format('DD/MM/YYYY')
+        $scope.selectedTime = $scope.selectedDatetime.format('HH:mm');
     }
 
-    $scope.dateChange = function () {
+    $scope.reset();
+
+    $scope.datetimeChange = function () {
         var date = $scope.selectedDate + ' ' + $scope.selectedTime;
         $log.info(date);
         $scope.selectedDatetime = moment(date, 'DD/MM/YYYY HH:mm');
     }
 });
 
-datepickerApp.directive('myDatepicker', function () {
-    return{
-        restrict: 'A',
-        link: function (scope, element, attrs) {
-            element.datepicker({
-                onClose: function (date) {
-                }
-            });
+datepickerApp
+    .directive('myTimepicker', function () {
+        return{
+            restrict: 'A',
+            link: function (scope, element) {
+                element.timepicker({
+                    scrollbar: true,
+                    timeFormat: 'HH:mm',
+                    change: function (time) {
+                        scope.selectedTime = moment(time).format('HH:mm');
+                        console.log(time);
+                        scope.datetimeChange();
+                        scope.$apply();
+                    }
+                });
+            }
         }
-    }
-})
+    })
+    .directive('myDatepicker', function () {
+        return{
+            restrict: 'A',
+            link: function (scope, element) {
+                element.datepicker({
+                    format: 'dd/m/yyyy',
+                    autoclose: true
+                })
+            }
+        }
+
+    })
+
