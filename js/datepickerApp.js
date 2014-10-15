67c1cddb7e330b0769df01d5b1fc13b5efda913b
zeng.stephen@gmail.com <zeng.stephen@gmail.com>
2014-10-15 23:01:30 +0800

    Adding datetime picker

--- js/datepickerApp.js
+++ js/datepickerApp.js
@@ -4,13 +4,33 @@
 'use strict'
 var datepickerApp = angular.module('datepickerApp', []);
 
-$('input.datepicker').datepicker();
-$('input.timepicker').timepicker({scrollbar: true});
+$('input.datepicker').datepicker({format: 'dd/m/yyyy'});
+$('input.timepicker').timepicker({scrollbar: true, timeFormat: 'HH:mm'});
 
-datepickerApp.controller('datepickerController', function ($scope) {
-    $scope.selectedDatetime = new Date();
+datepickerApp.controller('datepickerController', function ($scope, $log) {
+    $scope.selectedDatetime = moment();
+    $scope.selectedDate = $scope.selectedDatetime.format('DD/MM/YYYY')
+    $scope.selectedTime = $scope.selectedDatetime.format('HH:mm');
 
     $scope.reset = function () {
-        $scope.selectedDatetime = new Date();
+        $scope.selectedDatetime = moment();
+    }
+
+    $scope.dateChange = function () {
+        var date = $scope.selectedDate + ' ' + $scope.selectedTime;
+        $log.info(date);
+        $scope.selectedDatetime = moment(date, 'DD/MM/YYYY HH:mm');
     }
 });
+
+datepickerApp.directive('myDatepicker', function () {
+    return{
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            element.datepicker({
+                onClose: function (date) {
+                }
+            });
+        }
+    }
+})
